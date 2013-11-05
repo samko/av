722fffb5e2a80782face32e6faa54ec221475999
samko <infossam@yahoo.fr>
2013-11-05 13:33:32 +0100

    Initial Git repo with RVM directives 	 before using Gitflow tool: 		git flow init 		git push origin develop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Av::Application.config.secret_key_base = 'c12309b3438c1884fe0112af831fd6f64897c348fbdc067c5244805b9444351a3c031e67ede70935b45c58272e690788c2306c8a677388cf79a51f9e1b507af8'
